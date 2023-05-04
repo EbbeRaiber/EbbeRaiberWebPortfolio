1c5b647ada83f18063e462dc5cb1427058a7a80b
EbbeRaiber <ebberaiber@outlook.com>
2023-05-03 21:04:08 -0400

    finally maybe

--- src/objects.js
+++ src/objects.js
@@ -9,8 +9,10 @@ const Objects = () => {
 
     useEffect(() => {
         //creating a new scene and setting it to our scene
+        const width = window.innerWidth;
+        const height = window.innerHeight;
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
         const renderer = new THREE.WebGLRenderer({ 
             canvas: document.querySelector('#bg')
         });
@@ -33,6 +35,17 @@ const Objects = () => {
             controls.update();
             };
         animate();
+
+        function onWindowResize() {
+            const width = window.innerWidth;
+            const height = window.innerHeight;
+            renderer.setSize(width, height);
+            camera.aspect = width / height;
+            camera.updateProjectionMatrix();
+            renderer.render(scene, camera);
+          }
+
+        window.addEventListener("resize", onWindowResize);
 
         canvasRef.current.appendChild(renderer.domElement);
 
